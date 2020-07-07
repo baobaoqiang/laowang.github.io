1dc851eaa9c147b6abe18d4ef920dfd43f965608
wangq <89414266@qq.com>
2020-07-07 09:46:46 +0800

    Updates

---
+++ precache-manifest.5588a94bfe7adfd54d743553a57f4732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc3b730c19b862d77ff805e723fc2ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e7de105157084721b77",
+    "url": "/static/css/2.290d5372.chunk.css"
+  },
+  {
+    "revision": "ad315d78f01047c2e204",
+    "url": "/static/css/main.e5b6209f.chunk.css"
+  },
+  {
+    "revision": "6e7de105157084721b77",
+    "url": "/static/js/2.e9a05ec6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e9a05ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad315d78f01047c2e204",
+    "url": "/static/js/main.ea17f94c.chunk.js"
+  },
+  {
+    "revision": "748b931ac798421b8c54",
+    "url": "/static/js/runtime-main.ca1ac2cc.js"
+  }
+]);
